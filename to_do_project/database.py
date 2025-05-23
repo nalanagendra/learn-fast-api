69faa94342a722df36a5461b26aa268376f4f493
nalanagendra <nalanagendrakamath@gmail.com>
2025-05-23 14:57:16 +0530

    Connect to different Databases:
    Postgres and mysql

--- to_do_project/database.py
+++ to_do_project/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL="sqlite:///./todosapp.db"
+# SQLALCHEMY_DATABASE_URL="sqlite:///./todosapp.db"
+# SQLALCHEMY_DATABASE_URL="postgresql://postgres:mroads123@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:mroads123@localhost:3306/TodoApplicationDatabase"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
